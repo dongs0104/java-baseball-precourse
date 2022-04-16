addb5b0c9904eb85f49a31a70610541b1b010339
Dong SHIN <dong_s@me.com>
2022-04-17 03:48:41 +0900

    feat: Init Game Class

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+
+import baseball.model.BaseBallGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseBallGame bbg = new BaseBallGame();
+        bbg.run();
     }
 }
---
+++ src/main/java/baseball/model/BaseBallGame.java
@@ -0,0 +1,24 @@
+package baseball.model;
+
+
+public class BaseBallGame {
+
+    public BaseBallGame() {
+    }
+
+    public void run() {
+        while (true) {
+            /*
+                1. 유효 난수 생성
+                2. 입력 프롬프트 출력
+                3. 입력값 검증 로직
+                4. 난수와 입력 값 비교
+                5. 힌트/종료 멘트 출력
+            */
+            break;
+        }
+    }
+
+    private void play(int[] answer) {
+    }
+}
